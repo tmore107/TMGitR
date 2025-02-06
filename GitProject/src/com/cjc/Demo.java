363eb0a3ec8e83ebbf4278985825995e28a8cf9e
Ajay Sathe <Ajay Sathe@LAPTOP-SCLIVQOO>
2025-02-06 15:11:40 +0530

    git By Trupti Sathe

--- GitProject/src/com/cjc/Demo.java
+++ GitProject/src/com/cjc/Demo.java
@@ -2,10 +2,4 @@ package com.cjc;
 
 public class Demo {
 
-	public static void main(String[] args) {
-		System.out.println("This is Demo Class");
-}
-public void m1(){
-	System.out.println("THis is m1 Method of Demo Class");
-}
 }
---
+++ GitProject/src/com/cjc/Pune.java
@@ -0,0 +1,10 @@
+package com.cjc;
+
+public class Pune {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Pune");
+
+	}
+
+}
