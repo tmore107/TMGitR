6da533b1287deeb888e52109b11a6abac101816a
tmore107 <moretrupti107@gmail.com>
2025-02-06 15:13:06 +0530

    Update Pune.java

--- GitProject/src/com/cjc/Pune.java
+++ GitProject/src/com/cjc/Pune.java
@@ -4,7 +4,9 @@ public class Pune {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Pune");
-
+    public void m1(){
+	    System.out.println("This is m1 method");
+    }
 	}
 
 }
