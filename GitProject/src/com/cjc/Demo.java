ffb8210e848ad0db60cbbca276de1d2b237378b4
tmore107 <moretrupti107@gmail.com>
2025-02-06 14:44:12 +0530

    Update Demo.java

--- GitProject/src/com/cjc/Demo.java
+++ GitProject/src/com/cjc/Demo.java
@@ -4,7 +4,8 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("This is Demo Class");
-
-	}
-
+}
+public void m1(){
+	System.out.println("THis is m1 Method of Demo Class");
+}
 }
